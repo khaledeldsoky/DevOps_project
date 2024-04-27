bdb00a6701a8428a84cb980c55d28e2b13692914
khaledeldsoky <khlaedmohamedeldsoky@gmail.com>
2024-04-27 22:53:16 +0300

    no chage

--- bash/hosts.sh
+++ bash/hosts.sh
@@ -1,5 +1,6 @@
 #   private key بتاع ال  path حط انت بقا انت ال 
-cat  <<-OEF  > [ path_to_ansible_hosts_file ]
+
+cat  <<-OEF  > /mnt/linux/data/project/devops_1/infra/ansible/hosts
 [bastion]
 $1 ansible_ssh_private_key_file=/mnt/linux/data/project/devops_1/infra/terrafrom/public_ec2_key_name.pem
 OEF
